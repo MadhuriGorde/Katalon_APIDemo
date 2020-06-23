4c067afcb3ec84bba73fa8b84f6c5c1b9c9b8d25
madhuri.gorde <madhuri.gorde@P3ALAP-21J1NW2.dir.svc.accenture.com>
2020-06-23 17:41:11 +0530

    First commit

---
+++ Object Repository/CalculatorSoapService/Addition.rs
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Addition</name>
+   <tag></tag>
+   <elementGuidId>abfe683a-3c5a-4e9a-a287-540b9658fb73</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>text/xml; charset=utf-8</value>
+   </httpHeaderProperties>
+   <katalonVersion>7.5.5</katalonVersion>
+   <restRequestMethod></restRequestMethod>
+   <restUrl></restUrl>
+   <serviceType>SOAP</serviceType>
+   <soapBody>&lt;soap:Envelope xmlns:soap=&quot;http://www.w3.org/2003/05/soap-envelope&quot; xmlns:tem=&quot;http://tempuri.org/&quot;>
+   &lt;soap:Header/>
+   &lt;soap:Body>
+      &lt;tem:Add>
+         &lt;tem:intA>3&lt;/tem:intA>
+         &lt;tem:intB>3&lt;/tem:intB>
+      &lt;/tem:Add>
+   &lt;/soap:Body>
+&lt;/soap:Envelope></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod>SOAP12</soapRequestMethod>
+   <soapServiceEndpoint>http://www.dneonline.com/calculator.asmx</soapServiceEndpoint>
+   <soapServiceFunction>Add</soapServiceFunction>
+   <useServiceInfoFromWsdl>false</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+
+assertThat(response.getResponseText()).contains('AddResult')
+
+
+WS.verifyElementPropertyValue(response, 'AddResponse.AddResult', '6')</verificationScript>
+   <wsdlAddress>http://www.dneonline.com/calculator.asmx?WSDL</wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d782d30a-3f8e-4f24-ad1c-339e7434f4b1</testSuiteGuid>
+   <testCaseLink>
+      <guid>159b9c2d-d625-4007-b4b1-e8d8730414f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33b5238b-1896-46e4-b7ca-0855beafc055</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>92e21e3b-3283-4b7c-b213-e41aa4c7802c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/HTTPMethod</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>638b2b6c-4cd1-4059-b633-4fe399b2f827</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/ListUserTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
